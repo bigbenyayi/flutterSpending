ea68711a6f56cdd7106b13daa5aa784a52f2dc99
Benjamin Corben <corbenbenjamin@gmail.com>
2019-11-16 16:51:42 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spending/widgets/new_transaction.dart';
+import 'package:flutter_spending/widgets/user_transaction.dart';
+
+import 'models/transaction.dart';
+import 'widgets/transaction_list.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text("CHART"),
+              elevation: 5,
+            ),
+          ),
+          UserTransactions()
+        ],
+      ),
+    );
+  }
+}
